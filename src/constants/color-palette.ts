ffc1938a1eb9f1cd1d75cbf626182111bef12d6f
Raj-techwondoe <raj.upadhyay@techwondoe.com>
2023-05-16 15:36:22 +0530

    7: shifted colors to constant, for internal use

---
+++ src/constants/color-palette.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+  primary: '#D0352F',
+  secondary: '#26272B',
+  dashboardBg: '#18181B',
+  dashboardWhite: '#FFFFFF',
+  dashboardWhite100: '#F4F4F5',
+  tableCell: '#26272B',
+  tableCellText: '#EBEBEB',
+  tableHeader: '#1E1E21',
+  iconBg: '#3F3F46',
+  cellDividerStoke: ' #424242',
+  body: '#A6A6A6',
+  formBg: '#2D2D2D',
+  headerButtonHover: ' #FF0000',
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
+import { COLORS } from './src/constants/color-palette';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: '#D0352F',
-        secondary: '#26272B',
-        dashboardBg: '#18181B',
-        dashboardWhite: '#FFFFFF',
-        dashboardWhite100: '#F4F4F5',
-        tableCell: '#26272B',
-        tableCellText: '#EBEBEB',
-        tableHeader: '#1E1E21',
-        iconBg: '#3F3F46',
-        cellDividerStoke: ' #424242',
-        body: '#A6A6A6',
-        formBg: '#2D2D2D',
-        headerButtonHover: ' #FF0000',
+        ...COLORS,
       },
     },
   },
