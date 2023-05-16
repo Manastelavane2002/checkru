414967995cab57038a3ac9936a2156524499e503
Raj-techwondoe <raj.upadhyay@techwondoe.com>
2023-05-16 15:44:42 +0530

    7: Enhanced Icons

--- src/components/global/Icon/Icon.tsx
+++ src/components/global/Icon/Icon.tsx
@@ -4,12 +4,22 @@ import { IconTypes } from './Icon.types';
 
 interface Props {
   color?: string;
+  fill?: string;
   name: IconTypes;
   onClick?: () => void;
   size?: number;
 }
-
-export function Icon({ name, color = 'white', size = 20, onClick }: Props) {
+// eg: <Icon name='arrow-left' size={20} color={COLORS.dashboardWhite}/>
+export function Icon({ name, color = 'white', fill, size = 20, onClick }: Props) {
   const SVGIcon: ElementType = iconPack[name];
-  return <SVGIcon width={size} height={size} color={color} onClick={onClick} />;
+  return (
+    <SVGIcon
+      width={size}
+      height={size}
+      color={color}
+      fill={fill}
+      stroke={color}
+      onClick={onClick}
+    />
+  );
 }
