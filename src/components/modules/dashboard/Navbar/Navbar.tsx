70b0474273308241e05d5a6fcfacf66120e44ef6
Manas <manas.telavane@techwondoe.com>
2023-05-17 10:38:45 +0530

    18-Make-Header-and-Navbar-responsive

--- src/components/modules/dashboard/Navbar/Navbar.tsx
+++ src/components/modules/dashboard/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export function Navbar({ navElements }: Props) {
   return (
-    <nav className="flex justify-between items-center bg-secondary font-semibold text-white inter text-base px-[112px] py-[22px]">
+    <nav className="flex lg:justify-between items-center bg-secondary font-semibold text-white inter text-base px-[112px] py-[22px] gap-10 overflow-x-auto whitespace-nowrap">
       {navElements.map((element, index) => {
         return (
           <Link href={element.link} className="hover:text-body" key={index}>
