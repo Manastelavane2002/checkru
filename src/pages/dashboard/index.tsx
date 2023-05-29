c1769eeb0c1124a9a8e19d4e620d35af606e485a
Raj-techwondoe <raj.upadhyay@techwondoe.com>
2023-05-29 12:35:13 +0530

    2: passed statement props

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,13 +5,15 @@ import DashboardPage from 'src/components/pages/Dashboard/Dashboard';
 import { checkSSRTokenAndRedirect } from 'src/utils/auth-redirect';
 
 interface StatementPageProps {
+  accountInfo: null;
   statements: Array<Statement>;
+  transactions: null;
 }
 
 export const getServerSideProps: GetServerSideProps<{}> = async (
   context: GetServerSidePropsContext
 ) => {
-  let pageProps: StatementPageProps = { statements: [] };
+  let pageProps: StatementPageProps = { statements: [], accountInfo: null, transactions: null };
   const statementsResponse = await getStatementDataBackend();
   if (statementsResponse.data && statementsResponse.isSuccess) {
     pageProps = {
