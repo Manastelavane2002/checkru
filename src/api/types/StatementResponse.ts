6a22732e6260332975292286bb3a5e2e342b3dab
Raj-techwondoe <raj.upadhyay@techwondoe.com>
2023-05-29 12:33:46 +0530

    2: passed statement props

--- src/api/types/StatementResponse.ts
+++ src/api/types/StatementResponse.ts
@@ -1,16 +1,17 @@
 import { CommonResponse } from './CommonResponse';
 
+export interface Statement {
+  availableCredit: number;
+  creditLimit: number;
+  endDate: string;
+  minAmountDue: number;
+  paymentDueDate: string;
+  startDate: string;
+  statementPeriod: string;
+  totalAmountDue: number;
+}
 export interface StatementResponse extends CommonResponse {
   data: {
-    statements: Array<{
-      availableCredit: number;
-      creditLimit: number;
-      endDate: string;
-      minAmountDue: number;
-      paymentDueDate: string;
-      startDate: string;
-      statementPeriod: string;
-      totalAmountDue: number;
-    }>;
+    statements: Array<Statement>;
   };
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,31 @@
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { getStatementDataBackend } from 'src/api/backend/statement';
+import { Statement } from 'src/api/types';
 import DashboardPage from 'src/components/pages/Dashboard/Dashboard';
 import { checkSSRTokenAndRedirect } from 'src/utils/auth-redirect';
+
+interface StatementPageProps {
+  statements: Array<Statement>;
+}
 
 export const getServerSideProps: GetServerSideProps<{}> = async (
   context: GetServerSidePropsContext
 ) => {
-  return checkSSRTokenAndRedirect({ context });
+  let pageProps: StatementPageProps = { statements: [] };
+  const statementsResponse = await getStatementDataBackend();
+  if (statementsResponse.data && statementsResponse.isSuccess) {
+    pageProps = {
+      ...pageProps,
+      statements: statementsResponse.data.statements,
+    };
+  }
+  return checkSSRTokenAndRedirect({
+    context,
+    pageProps: pageProps as unknown as Record<string, string>,
+  });
 };
 
-export default function Dashboard() {
+export default function Dashboard(props: StatementPageProps) {
+  console.info(props);
   return <DashboardPage />;
 }
